acae5db79c80568f18e7d0c1c6bd601e1b7cb929
Sam1cesammy <samuel.asegame@gmail.com>
2023-09-11 06:53:54 -0700

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -11,11 +11,11 @@
 
 int main(void)
 {
-        char i;
+	char i;
 
-        for (i = 0; i < 10; i++)
-        printf("%d", i);
-        printf("\n");
+	for (i = 0; i < 10; i++)
+	printf("%d", i);
+	printf("\n");
 
-        return (0);
+	return (0);
 }
